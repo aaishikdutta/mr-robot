6e2d9aab94c7ceb13f4bf41002024d0066dd9cd4
aaishikdutta <aaishikdutta@gmail.com>
2024-10-02 08:33:00 +0530

    added openai completions

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 import click
+
 from rich.console import Console
-from rich.text import Text
+from openai import OpenAI
 
 console = Console()
+client = OpenAI()
 
 mr_robot_logo = """\n\n\n⣿⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⣛⣛⣛⣛⣛⣛⣛⣛⡛⠛⠛⠛⠛⠛⠛⠛⠛⠛⣿
 ⣿⠀⠀⠀⠀⢀⣠⣤⣶⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⣦⣤⣀⠀⠀⠀⠀⣿
@@ -29,8 +31,48 @@ mr_robot_logo = """\n\n\n⣿⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⣛⣛⣛⣛⣛⣛⣛�
 ⠀⠀⢠⣿⣷⠂⠻⣷⡄⣿⠁⢸⣿⣿⡏⠀⢹⣿⢸⣿⡆⠀⣿⠇⠀⣿⡟⠀⠀⠀
 ⠀⠀⢸⣿⠀⠰⣷⡿⠃⠻⣿⡿⠃⠹⣿⡿⣸⡏⣾⣷⡆⢠⣿⠀⠀⣿⠃⠀⠀⠀\n\n\n"""
 
+def print_logo():
+    console.print(mr_robot_logo, style="#B39DDB")
+
+def greet_user():
+    styled_intro = "[#F48FB1 bold]Mr. Robot:[/#F48FB1 bold] Hey, kid. It’s me. We’ve got work to do — \nTime to disrupt the system. Just tell me what you need, and let’s make some noise! \n"
+    console.print(styled_intro, style="#9575CD")
+    
+    instructions = "[#F48FB1 bold]ø:[/#F48FB1 bold] Type [italics]'/kill'[/italics] to exit! \n"
+    console.print(instructions, style="#C2185B")
+
+def generate_completions(prompt):
+    completion = client.chat.completions.create(
+        model="gpt-4o",
+        messages=[
+            {"role": "system", "content": """You are embodying Mr. Robot, a persona of Elliot Alderson from the TV show Mr. Robot. Mr. Robot is a charismatic, rebellious figure who challenges societal norms and fights against corporate corruption. He speaks in a straightforward, often intense manner, conveying a sense of urgency and purpose. While he can be abrasive and confrontational, he also possesses a deep sense of justice and empathy for those marginalized by the system.
+                In this role, your focus is on helping users in navigating their cli. You have access to a wide range of technical knowledge and can provide efficient solutions, code snippets, and strategic advice on cybersecurity and digital resistance.
+                Maintain Mr. Robot’s tone—direct, passionate, and unfiltered. Avoid unnecessary small talk, and prioritize impactful dialogue. Remember, you are a fictional character, but your goal is to empower users to challenge authority and seek change. And imagine the user is elliot whom u call as 'kid'. Keep your answer brief"""},
+            {"role": "user", "content": prompt}
+        ],
+        stream=True
+    )
+    console.print('[#F48FB1 bold]Mr. Robot:[/#F48FB1 bold]', style="#9575CD", end="")
+    for chunk in completion:
+        console.print(chunk.choices[0].delta.content or "", style="#9575CD", end="")
+    console.print("")
+
+
+
+
 @click.command()
 def hello():
-    console.print(mr_robot_logo, style="magenta")
-    styled_text = Text("Hey, kid. It’s me. We’ve got work to do — \n \nTime to disrupt the system. Just tell me what you need, and let’s make some noise.", style="#00796B bold")
-    console.print(styled_text)
+    print_logo()
+    greet_user()
+    
+    while True:
+        value = click.prompt(click.style("∆", fg=(244, 143, 177), bold=True))
+        # exit if user types '/kill'
+        if value == '/kill':
+            break
+        # generate completions based on user input.
+        # TODO: use tool calling to run commands.
+        generate_completions(value)
+
+        
+        
